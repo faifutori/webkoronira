9d6f89de805aa7118b9dc803aba15fa2c4a3b907
faifutori <yudai4520@icloud.com>
2022-11-10 17:43:05 +0900

    元１

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.title').bgSwitcher({
+        images: ['../images/title1.png','../images/title2.png','../images/title3.png','../images/title4.png'],
+
+        interval: 7000,
+        loop: true
+    });
+
+}); 
+
+
+
